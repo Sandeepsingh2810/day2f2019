36e87a45784ae9338f77080c8a716a1cb22556fd
MacStudent <macstudent@126-iMac08.local>
2019-09-19 19:03:26 -0400

    Initial commit

---
+++ src/main/java/com/lambton/Arithmetic.java
@@ -0,0 +1,40 @@
+package com.lambton;
+
+public class Arithmetic
+{
+    int add(int a, int b)
+    {
+        int c;
+        c=a+b;
+        return c;
+    }
+
+    int sub(int a, int b)
+    {
+        int c;
+        c=a-b;
+        return c;
+    }
+
+    int mul(int a, int b)
+    {
+        int c;
+        c=a*b;
+        return c;
+    }
+    float div(int a, int b)
+    {
+         float c;
+        c= (float) a/b;
+        return c;
+    }
+
+    int mod(int a, int b)
+    {
+        int c;
+        c=a%b;
+        return c;
+    }
+
+
+}
---
+++ src/main/java/com/lambton/CollegeExample.java
@@ -0,0 +1,16 @@
+package com.lambton;
+
+public class CollegeExample
+{
+    public static void main(String[] args)
+    {
+    Arithmetic a1 = new Arithmetic();
+
+        System.out.println("Add " + a1.add(10,10));
+        System.out.println("Sub " + a1.sub(10,20));
+        System.out.println("Mul " + a1.mul(10,20));
+        System.out.println("Div " + a1.div(20,3));
+        System.out.println("Mod " + a1.mod(10,20));
+
+    }
+}
