da4665490949dc1ac65248b124e2c24036b7ccc1
macstudent <sandeepjangra0100@gmail.com>
2019-09-19 19:10:51 -0400

    create student class

---
+++ src/main/java/com/lambton/Student.java
@@ -0,0 +1,12 @@
+package com.lambton;
+
+public class Student
+{
+    Integer studentId;
+     String firstName;
+     String lastName;
+     int[] Markof5 ;
+     int total;
+     double Percentage;
+     String Results;
+}
